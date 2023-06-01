b040e111846bec90b66b46b600fd93968b923dc5
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-01 15:14:20 +0000

    import ship class and instantiate new object

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './styles/style.scss';
-console.log('hello');
+import Ship from './modules/ship';
+
+const ship = new Ship(3);
+console.log(ship.length);
+console.log(ship.hits);
+console.log(ship.isSunk());
+console.log(ship.hit(0));
+console.log(ship.hit(1));
+console.log(ship.hit(2));
+console.log(ship.hits);
+console.log(ship.isSunk());
