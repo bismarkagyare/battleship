43693734046695ba0c4343bee4c96766699f3b14
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-02 11:23:31 +0000

    add the overlap logic

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -19,7 +19,32 @@ class Gameboard {
       const newShip = new Ship(length);
       // check for overlap with existing ships
 
-      const overlappingShip = this.ships.find((ship) => {});
+      const overlappingShip = this.ships.find((ship) => {
+        if (orientation === 'horizontal') {
+          for (let i = 0; i < length; i++) {
+            if (ship.coordinates.row === row && ship.coordinates.column + i === column) {
+              return true;
+            } else if (orientation === 'vertical') {
+              for (let i = 0; i < length; i++) {
+                if (ship.coordinates.row + i === row && ship.coordinates.column === column) {
+                  return true;
+                }
+              }
+            }
+          }
+          return false;
+        }
+      });
+
+      if (!overlappingShip) {
+        newShip.coordinates = { column, row };
+        newShip.orientation = orientation;
+        this.ships.push(newShip);
+      } else {
+        console.log('there was an overlap');
+      }
+    } else {
+      console.log('invalid coordinates or orientation');
     }
   }
 }
