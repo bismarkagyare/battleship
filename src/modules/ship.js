7db8a4a1ae817da7c819980e0b505395c9c2ac30
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-02 17:49:00 +0000

    add a method to get index of cell in receiveAttack

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -11,6 +11,27 @@ class Ship {
   isSunk() {
     return this.hits.every((cell) => cell === true);
   }
+
+  // getIndexOfCell(row, column) {
+  //   const { length } = this;
+  //   const startColumn = this.coordinates.column;
+  //   const startRow = this.coordinates.row;
+
+  //   if (this.orientation === 'horizontal') {
+  //     return column - startColumn;
+  //   } else if (this.orientation === 'vertical') {
+  //     return row - startRow;
+  //   }
+  //   return -1;
+  // }
+
+  getIndexOfCell(row, column) {
+    const startColumn = this.coordinates.column;
+    const startRow = this.coordinates.row;
+    const index = this.orientation === 'horizontal' ? column - startColumn : row - startRow;
+
+    return index >= 0 && index < this.length ? index : -1;
+  }
 }
 
 export default Ship;
