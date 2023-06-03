2b2f8d4104ed5cfcd25826ad189c789057668583
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-03 22:02:39 +0000

    check if missed attacks are recorded

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -16,4 +16,19 @@ describe('Gameboard', () => {
     gameboard.placeShip(3, { column: 11, row: 2 }, 'horizontal');
     expect(gameboard.ships.length).toBe(0);
   });
+
+  test('receiving an attack on a ship should register a hit', () => {
+    const ship = new Ship(3);
+    ship.coordinates = { column: 2, row: 3 };
+    ship.orientation = 'horizontal';
+    gameboard.ships.push(ship);
+
+    gameboard.receiveAttacks({ column: 2, row: 3 });
+    expect(ship.hits).toEqual([true, false, false]);
+  });
+
+  test('receiving an attack on a missed location should add to missedAttacks', () => {
+    gameboard.receiveAttacks({ column: 5, row: 7 });
+    expect(gameboard.missedAttacks.length).toBe(1);
+  });
 });
