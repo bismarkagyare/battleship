77db7fbdd4ae12ca234ceef9b89c7f3fb9b078c1
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-04 20:43:29 +0000

    initialiase makeAttack method

--- src/modules/player.js
+++ src/modules/player.js
@@ -4,7 +4,13 @@ class Player {
   constructor(name) {
     this.name = name;
     this.gameboard = new Gameboard(10);
-    this.isTurn = false;
+    this.isPlayerTurn = false;
+  }
+
+  makeAttack(gameboard) {
+    if (this.isPlayerTurn) {
+      const attackCoordinates = this.getPlayerAttackCoordinates();
+    }
   }
 }
 
