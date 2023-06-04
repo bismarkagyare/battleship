11ae1926eda14a2e2a25372a058263cde3234511
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-04 13:29:33 +0000

    implement test cases for getting the index of cells

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -33,4 +33,20 @@ describe('Ship', () => {
     ship.hit(2);
     expect(ship.isSunk()).toBe(true);
   });
+
+  test('getIndexOfCell returns the correct index for a horizontal ship', () => {
+    ship.coordinates = { column: 2, row: 3 };
+    ship.orientation = 'horizontal';
+    expect(ship.getIndexOfCell(4, 3)).toBe(2);
+    expect(ship.getIndexOfCell(3, 3)).toBe(1);
+    expect(ship.getIndexOfCell(1, 3)).toBe(-1); // outside the ship's range
+  });
+
+  test('getIndexOfCell returns the correct index for a vertical ship', () => {
+    ship.coordinates = { column: 5, row: 2 };
+    ship.orientation = 'vertical';
+    expect(ship.getIndexOfCell(5, 3)).toBe(1);
+    expect(ship.getIndexOfCell(5, 4)).toBe(2);
+    expect(ship.getIndexOfCell(0, 0)).toBe(-1); // outside the ship's range
+  });
 });
