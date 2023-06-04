326aec4ff440ce0390276d7aff15bc73c0f33322
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-04 17:14:23 +0000

    initialise the constructor with the some properties

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,3 +1,11 @@
+import Gameboard from './gameboard';
+
 class Player {
-  constructor(params) {}
+  constructor(name) {
+    this.name = name;
+    this.gameboard = new Gameboard(10);
+    this.isTurn = false;
+  }
 }
+
+export default Player;
