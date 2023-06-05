b2927b5f64388e14061f4e3415bcee660c1fd8bc
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-05 18:44:10 +0000

    refactor makeAttack method to ensure correct turn switching

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,24 +1,34 @@
 import Gameboard from './gameboard';
 
 class Player {
-  constructor(name) {
+  constructor(name, gameboard) {
     this.name = name;
-    this.gameboard = new Gameboard(10);
+    this.gameboard = gameboard || new Gameboard(10);
     this.isPlayerTurn = true;
     this.isComputerTurn = false;
   }
 
-  makeAttack(gameboard) {
-    if (this.isPlayerTurn) {
-      const attackCoordinates = this.getPlayerAttackCoordinates();
-      gameboard.receiveAttacks(attackCoordinates);
-    } else if (this.isComputerTurn) {
-      const attackCoordinates = this.getComputerAttackCoordinates;
-      gameboard.receiveAttacks(attackCoordinates);
-    }
+  // makeAttack(gameboard) {
+  //   if (this.isPlayerTurn) {
+  //     const attackCoordinates = this.getPlayerAttackCoordinates();
+  //     this.gameboard.receiveAttacks(attackCoordinates);
+  //   } else if (this.isComputerTurn) {
+  //     const attackCoordinates = this.getComputerAttackCoordinates;
+  //     this.gameboard.receiveAttacks(attackCoordinates);
+  //   }
+  //   this.switchTurns();
+  // }
+
+  makeAttack() {
+    const attackCoordinates = this.isPlayerTurn
+      ? this.getPlayerAttackCoordinates()
+      : this.getComputerAttackCoordinates();
+
+    this.gameboard.receiveAttacks(attackCoordinates);
     this.switchTurns();
   }
 
+  //gonna pull it from the dom.js or html: depends
   getPlayerAttackCoordinates() {}
 
   getComputerAttackCoordinates() {
