4489e8683383ebe51a09044394d61e757701a9dd
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-07 15:06:25 +0000

    remove console.logs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import './styles/style.scss';
 import Ship from './modules/ship';
 
-const ship = new Ship(3);
-console.log(ship.length);
-console.log(ship.hits);
-console.log(ship.isSunk());
-console.log(ship.hit(0));
-console.log(ship.hit(1));
-console.log(ship.hit(2));
-console.log(ship.hits);
-console.log(ship.isSunk());
+console.log('hello world');
