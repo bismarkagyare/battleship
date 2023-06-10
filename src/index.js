081c717d09130e1ec34a02d319cf174251d32ec6
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-10 13:14:31 +0000

    initialise gameboard objects

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import './styles/style.scss';
 import Ship from './modules/ship';
-import Gameboard from './modules/gameboard';
-import renderGameboard from './modules/dom';
+import { initialiseGameboard } from './modules/dom';
 
-const playerBoardContainer = document.querySelector('.player-board');
-const computerBoardContainer = document.querySelector('.computer-board');
-
-const playerGameboard = new Gameboard(10);
-const computerGameboard = new Gameboard(10);
-
-renderGameboard(playerBoardContainer, playerGameboard);
-renderGameboard(computerBoardContainer, computerGameboard);
+initialiseGameboard();
