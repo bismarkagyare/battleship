273e48763a84cab822d1462ca9d6783191973bd0
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-11 17:28:45 +0000

    remove getIndexOfCell method for now

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,7 +1,7 @@
 class Ship {
   constructor(length) {
     this.length = length;
-    this.hits = new Array(length).fill(false);
+    this.hits = Array(length).fill(false);
   }
 
   hit(index) {
@@ -9,20 +9,7 @@ class Ship {
   }
 
   isSunk() {
-    return this.hits.every((cell) => cell === true);
-  }
-
-  getIndexOfCell(column, row) {
-    const columnIndex = column - this.coordinates.column;
-    const rowIndex = row - this.coordinates.row;
-
-    if (this.orientation === 'horizontal' && rowIndex === 0) {
-      return columnIndex >= 0 && columnIndex < this.length ? columnIndex : -1;
-    } else if (this.orientation === 'vertical' && columnIndex === 0) {
-      return rowIndex >= 0 && rowIndex < this.length ? rowIndex : -1;
-    }
-
-    return -1;
+    return this.hits.every((hit) => hit);
   }
 }
 
