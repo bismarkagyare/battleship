3e273b535325f8fbb1fc0770e25ea950e9587c14
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-11 17:28:10 +0000

    comment out placeRandomShips function

--- src/modules/shipsPlacement.js
+++ src/modules/shipsPlacement.js
@@ -1,26 +1,29 @@
-const placeShipRandomly = (gameboard) => {
-  const shipLength = [5, , 4, 3, 2, 1];
-  const orientations = ['horizontal', 'vertical'];
+// const placeShipRandomly = (gameboard) => {
+//   const shipLength = [5, 4, 3, 2, 1];
+//   const orientations = ['horizontal', 'vertical'];
 
-  shipLength.forEach((length) => {
-    const orientation = orientations[Math.floor(Math.random() * orientations.length)];
-    let coordinates = getRandomCoordinates(gameboard.size, length, orientation);
+//   shipLength.forEach((length) => {
+//     const orientation = orientations[Math.floor(Math.random() * orientations.length)];
+//     let coordinates = getRandomCoordinates(gameboard.size, length, orientation);
 
-    while (!gameboard.isValidPosition(length, coordinates, orientation)) {
-      coordinates = getRandomCoordinates(gameboard.size, length, orientation);
-    }
+//     let maxAttempts = 100;
 
-    gameboard.placeShip(length, coordinates, orientation);
-  });
-};
+//     while (!gameboard.isValidPosition(length, coordinates, orientation) && maxAttempts > 0) {
+//       coordinates = getRandomCoordinates(gameboard.size, length, orientation);
+//       maxAttempts--;
+//     }
 
-const getRandomCoordinates = (size, length, orientation) => {
-  const maxRow = size - (orientation === 'horizontal' ? 1 : length);
-  const maxCol = size - (orientation === 'vertical' ? 1 : length);
-  const row = Math.floor(Math.random() * maxRow);
-  const col = Math.floor(Math.random() * maxCol);
+//     gameboard.placeShip(length, coordinates, orientation);
+//   });
+// };
 
-  return { col, row };
-};
+// const getRandomCoordinates = (size, length, orientation) => {
+//   const maxRow = size - (orientation === 'horizontal' ? 1 : length - 1);
+//   const maxCol = size - (orientation === 'vertical' ? 1 : length - 1);
+//   const row = Math.floor(Math.random() * maxRow);
+//   const col = Math.floor(Math.random() * maxCol);
 
-export default placeShipRandomly;
+//   return { col, row };
+// };
+
+// export default placeShipRandomly;
