52bad329a3ad6a2036ef080840b426caded5ee95
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-11 17:30:25 +0000

    refactor renderGameboard function and initialise gameboard

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,28 +1,78 @@
 import Gameboard from './gameboard';
+import Ship from './ship';
 
-const renderGameboard = (container, gameboard) => {
+const initializeGame = () => {
+  const playerGameboard = new Gameboard(10);
+  const computerGameboard = new Gameboard(10);
+
+  // Place player's ships
+  const playerShips = [
+    new Ship(5), // Carrier
+    new Ship(4), // Battleship
+    new Ship(3), // Cruiser
+    new Ship(3), // Submarine
+    new Ship(2), // Destroyer
+  ];
+  placeShipsRandomly(playerGameboard, playerShips);
+
+  // Place computer's ships
+  const computerShips = [new Ship(5), new Ship(4), new Ship(3), new Ship(3), new Ship(2)];
+  placeShipsRandomly(computerGameboard, computerShips);
+
+  // Render the gameboards
+  renderGameboard('player-board', playerGameboard);
+  renderGameboard('computer-board', computerGameboard);
+};
+
+const placeShipsRandomly = (gameboard, ships) => {
+  for (const ship of ships) {
+    let startRow, startCol, orientation;
+
+    do {
+      startRow = getRandomCoordinate(gameboard.size);
+      startCol = getRandomCoordinate(gameboard.size);
+      orientation = getRandomOrientation();
+    } while (!gameboard.placeShip(ship, startRow, startCol, orientation));
+  }
+};
+
+const getRandomCoordinate = (size) => {
+  return Math.floor(Math.random() * size);
+};
+
+const getRandomOrientation = () => {
+  return Math.random() < 0.5 ? 'horizontal' : 'vertical';
+};
+
+const renderGameboard = (containerId, gameboard) => {
+  const container = document.getElementById(containerId);
   container.innerHTML = '';
 
   for (let row = 0; row < gameboard.size; row++) {
     for (let col = 0; col < gameboard.size; col++) {
       const cell = document.createElement('div');
       cell.classList.add('board-cell');
-      cell.dataset.column = col;
       cell.dataset.row = row;
+      cell.dataset.column = col;
+
+      const ship = gameboard.getShipAt(row, col);
+      if (ship !== null) {
+        cell.classList.add('ship-cell');
+      }
+
+      const orientation = gameboard.getOrientationAt(row, col);
+      const isHorizontal = orientation === 'horizontal';
+      const isVertical = orientation === 'vertical';
+
+      if (isHorizontal) {
+        cell.classList.add('ship-cell-horizontal');
+      } else if (isVertical) {
+        cell.classList.add('ship-cell-vertical');
+      }
+
       container.appendChild(cell);
     }
   }
 };
 
-const initialiseGameboard = () => {
-  const playerBoardContainer = document.querySelector('.player-board');
-  const computerBoardContainer = document.querySelector('.computer-board');
-
-  const playerGameboard = new Gameboard(10);
-  const computerGameboard = new Gameboard(10);
-
-  renderGameboard(playerBoardContainer, playerGameboard);
-  renderGameboard(computerBoardContainer, computerGameboard);
-};
-
-export { renderGameboard, initialiseGameboard };
+export default initializeGame;
