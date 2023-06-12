3ed03ce532fbffa6b7a0826cb1bb89fcd3c6226e
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-12 14:20:24 +0000

    rewrite placeShipsRandomly function

--- src/modules/shipsPlacement.js
+++ src/modules/shipsPlacement.js
@@ -1,29 +1,21 @@
-// const placeShipRandomly = (gameboard) => {
-//   const shipLength = [5, 4, 3, 2, 1];
-//   const orientations = ['horizontal', 'vertical'];
+const placeShipsRandomly = (gameboard, ships) => {
+  ships.forEach((ship) => {
+    let startRow, startCol, orientation;
 
-//   shipLength.forEach((length) => {
-//     const orientation = orientations[Math.floor(Math.random() * orientations.length)];
-//     let coordinates = getRandomCoordinates(gameboard.size, length, orientation);
+    do {
+      startRow = getRandomCoordinate(gameboard.size);
+      startCol = getRandomCoordinate(gameboard.size);
+      orientation = getRandomOrientation();
+    } while (!gameboard.placeShip(ship, startRow, startCol, orientation));
+  });
+};
 
-//     let maxAttempts = 100;
+const getRandomCoordinate = (size) => {
+  return Math.floor(Math.random() * size);
+};
 
-//     while (!gameboard.isValidPosition(length, coordinates, orientation) && maxAttempts > 0) {
-//       coordinates = getRandomCoordinates(gameboard.size, length, orientation);
-//       maxAttempts--;
-//     }
+const getRandomOrientation = () => {
+  return Math.random() < 0.5 ? 'horizontal' : 'vertical';
+};
 
-//     gameboard.placeShip(length, coordinates, orientation);
-//   });
-// };
-
-// const getRandomCoordinates = (size, length, orientation) => {
-//   const maxRow = size - (orientation === 'horizontal' ? 1 : length - 1);
-//   const maxCol = size - (orientation === 'vertical' ? 1 : length - 1);
-//   const row = Math.floor(Math.random() * maxRow);
-//   const col = Math.floor(Math.random() * maxCol);
-
-//   return { col, row };
-// };
-
-// export default placeShipRandomly;
+export default placeShipsRandomly;
